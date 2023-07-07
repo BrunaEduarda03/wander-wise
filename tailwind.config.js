c351f34e6009f359a48c2275a16b0a220b907459
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-07-07 20:55:27 -0300

    chore:add tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "search-background": "url(/world-map.png)",
+      },
+      colors: {
+        primary: "#590BD8",
+        primaryLighter: "#DDD5EA",
+        primaryDarker: "#312A4F",
+        grayPrimary: "#717171",
+        grayLighter: "#BBBFBF",
+        walterWhite: "#F5F5F5",
+      },
+      textColor: {
+        dark: "#717171",
+      },
+    },
   },
   plugins: [],
-}
-
+};
