d9697d79c3687b83316d01bcc50b8348924dcad2
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-07-11 17:26:45 -0300

    chore:update danger in button Component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ function Button({ className,variant='primary', ...props }: ButtonProps) {
   const variantClasses = {
     primary: 'bg-primary text-white hover:bg-primaryDarker',
     outlined: 'bg-transparent border-2 border-primary text-primary',
-    danger: 'bg-transparent border-2 border-redPrimary text-redPrimary',
+    danger: 'bg-transparent border-2 border-redPrimary text-redPrimary hover:bg-redPrimary hover:text-white',
   }
   const _className = twMerge(variantClasses[variant], "appearance-none rounded-lg p-2 text-sm font-medium shadow transition-all", className);
 
