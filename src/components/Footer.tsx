d61cfdf783c8fb86ccbe53a8ed4a5e411bccf3bf
Bruna Eduarda Cruz Maciel <brunaduda37@gmail.com>
2023-07-26 02:43:53 -0300

    chore:update footer for dark mode

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,12 +7,14 @@ import React from 'react'
 function Footer() {
   const { theme, setTheme } = useTheme();
   return (
-   
-      <div className='p-5 bg-walterWhite flex flex-col justify-center items-center'>
-      <Image src='/logo.png' height={23} width={133} alt='logo trips'  />
-      <p className='mt-1 font-medium text-sm'>Todos os direitos reservados!</p>
-      <p className='mt-1 font-normal text-sm'>Desenvolvido por Bruna Eduarda, 2023</p>
-    </div>
+    <div>
+      <div className="w-full h-[1px] bg-grayLighter mt-10 mb-3"></div>
+        <div className='p-5 flex flex-col justify-center items-center '>
+          <Image src='/logo.png' height={23} width={133} alt='logo trips'  />
+          <p className='mt-1 font-medium text-sm'>Todos os direitos reservados!</p>
+          <p className='mt-1 font-normal text-sm'>Desenvolvido por Bruna Eduarda, 2023</p>
+        </div>
+      </div>
     )
   
 }
